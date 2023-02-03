81cf7c830b1455b7c9c4fde083949b885dc98656
Guilherme Marques <71295632+GuilhermeMPCosta@users.noreply.github.com>
2023-02-03 09:24:18 -0300

    Olho mostra senha

--- login.js
+++ login.js
@@ -1,6 +1,14 @@
 function Mostra(){
     let a = document.querySelector(".senha")
+    let b = document.querySelector("#olho")
     a.type=a.type == 'text' ? 'password' : 'text'
+    if (a.type=='text'){
+        b.src='images/3844441-eye-see-show-view-watch_110305.svg'
+    }else{
+        b.src='images/3844443-disable-eye-inactive-see-show-view-watch_110296.svg'
+    }
+    
+
 }
 
 function login(){
